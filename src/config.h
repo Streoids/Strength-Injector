657afb282973a53815db0ef86b12ad2a9aa55c5a
adhi <129642486+Streoids@users.noreply.github.com>
2026-02-27 11:02:40 +0530

    names

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@ namespace Globals {
 
     inline char WORKING_DIR[1024]{};
     inline bool USE_CUSTOM_PROC_NAME = false;
-    inline const std::wstring NO_DLL_PATH_SELECTED_MSG{ L"Click \"Select\" to select a DLL file" };
+    inline const std::wstring NO_DLL_PATH_SELECTED_MSG{ L"No DLL selected" };
     inline std::wstring DLL_PATH{};
     inline const std::wstring MCBE_PROC_NAME{ L"Minecraft.Windows.exe" };
     inline std::wstring PROC_NAME{ Globals::MCBE_PROC_NAME };
